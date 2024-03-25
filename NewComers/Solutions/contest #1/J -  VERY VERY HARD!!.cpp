2a5af85578f66192dc1edfc1bdbe609c34e8c158
Samy Nagy <103966155+samynagy@users.noreply.github.com>
2024-03-25 20:05:35 +0200

    Create J -  VERY VERY HARD!!.cpp

---
+++ NewComers/Solutions/contest #1/J -  VERY VERY HARD!!.cpp
@@ -0,0 +1,20 @@
+/*
+   Author : Samy Nagy
+   Codeforces Handle  : [Samy_Nagy](https://codeforces.com/profile/Samy_Nagy)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    long long a, b;
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
