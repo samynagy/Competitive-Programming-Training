fdd8091f9f095d6ebd7400325932dbe54698f7b1
Samy Nagy <103966155+samynagy@users.noreply.github.com>
2024-03-25 19:53:44 +0200

    Create A-ICPC SHAC.cpp

---
+++ NewComers/Solutions/contest #1/A-ICPC SHAC.cpp
@@ -0,0 +1,18 @@
+/*
+   Author : Samy Nagy
+   Codeforces Handle  : [Samy_Nagy](https://codeforces.com/profile/Samy_Nagy)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    cout << "ICPC SHAC ";
+    return 0;
+}
