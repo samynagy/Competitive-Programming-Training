21506ae90058f2b832acb74953fbb93476906594
Saif Medhat <99921474+saifadin1@users.noreply.github.com>
2024-03-27 16:31:20 +0200

    Create K - can you solve this.cpp

---
+++ NewComers/week #5/sheet5 solutions/K - can you solve this.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);  
+  
+  int n , x;
+  cin >> n >> x;
+  cout << n / x;
+}
