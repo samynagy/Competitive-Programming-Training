ad0c6e80cfc4a44b75e93d7fcf9a4616aa904fbd
Saif Medhat <99921474+saifadin1@users.noreply.github.com>
2024-03-27 16:32:16 +0200

    Create M - Real madrid doesn't lose (Hard version).cpp

---
+++ NewComers/week #5/sheet5 solutions/M - Real madrid doesn't lose (Hard version).cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);  
+  
+  long long p;
+  cin >> p;
+  cout << p / 3 << '\n' << p % 3;
+}
